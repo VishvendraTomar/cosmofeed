43864422210af748ed571058e68c812c4a35073f
Vishvendra Tomar <vishvendratomar29@gmail.com>
2024-04-26 19:05:09 +0530

    redirecting evaluation

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,7 @@ function App() {
       //   ):(<Login onLogin = {handleLogin}/>
       //   )}
       //  </div>
-     <Router>
-         <switch>
-            <Route path='/' Component={mainroute}/>
-         </switch>
-     </Router>
+       <mainroute/>
   )
 }
 
